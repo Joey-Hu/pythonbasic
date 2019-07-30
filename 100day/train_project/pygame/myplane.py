dfed0acbee99a163acd64d1b83c7f9b89ecdb627
huhao <1925595006@qq.com>
2019-07-30 18:29:44 +0800

    飞机大战

---
+++ 100day/train_project/pygame/myplane.py
@@ -0,0 +1,17 @@
+# @author: huhao
+# @file: myplane.py
+# @time: 2019/7/30 18:19
+# @Document：https://www.python.org/doc/
+# @desc:
+
+import pygame
+
+# fuction:
+# __init__,
+# moveUp,
+# moveDown,
+# moveLeft,
+# moveRight,
+# reset
+class Myplane(pygame.sprite.Sprite):
+    def __init__(self, bg_size):
